515d89ba1a652b1280a0691c61f1a187fbc18765
Qudus Olawale Bashiru <qbashy@gmail.com>
2024-06-03 16:44:26 +0100

    configured cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require('cors')
 
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
@@ -22,7 +23,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors)
 
 // routes
 app.use("/", indexRouter);
