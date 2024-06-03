f767252fc7b111e4e15d2878092c0584ddf632d3
Qudus Olawale Bashiru <qbashy@gmail.com>
2024-06-03 17:12:17 +0100

    fixed cors bug

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors)
 
 // routes
 app.use("/", indexRouter);
