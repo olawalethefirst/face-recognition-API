e16d159e892528769dc95a8fe6e7df51242fc6aa
olawalethefirst <qbashy@gmail.com>
2024-06-09 12:25:53 +0100

    added user to signup and signin routes response

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,7 +5,7 @@ const { createUser, findUserByEmail } = UserModel;
 const { createTransaction } = require("../APIs/database");
 const { sendErrorResponse, sendSuccessResponse } = require("../utils/response");
 const { errorMessages } = require("../constants");
-const { generateToken } = require("../APIs/jwt")
+const { generateToken } = require("../APIs/jwt");
 
 class AuthController {
   static async registerUser(req, res) {
@@ -19,18 +19,21 @@ class AuthController {
       await registerPassword(loginData, transactionRef);
 
       const newUser = await createUser(user, transactionRef);
-      
-      // remove user identifiable personal data
-      delete newUser.name;
-      delete newUser.email;
 
-      const token = generateToken(newUser);
+      const tokenPayload = { ...newUser };
+      // remove user identifiable personal data
+      delete tokenPayload.name;
+      delete tokenPayload.email;
+      const token = generateToken(tokenPayload);
 
       transactionRef.commit();
-      
-      return sendSuccessResponse(res, "Registered user successfully", { token });
+
+      return sendSuccessResponse(res, "Registered user successfully", {
+        token,
+        user: newUser,
+      });
     } catch (error) {
-      console.error({name:"Error registering user", error})
+      console.error({ name: "Error registering user", error });
 
       transactionRef.rollback();
 
@@ -43,26 +46,28 @@ class AuthController {
 
     try {
       const validationResult = await validatePassword(email, password);
-      const user = await findUserByEmail(email)
+      const user = await findUserByEmail(email);
 
       if (user) {
+        const tokenPayload = { ...user };
         // remove user identifiable personal data
-        delete user.name;
-        delete user.email;
-
-        const token = generateToken(user)
+        delete tokenPayload.name;
+        delete tokenPayload.email;
+        const token = generateToken(tokenPayload);
 
         if (validationResult.successful) {
-          return sendSuccessResponse(res, "Signed in successfully", { token });
+          return sendSuccessResponse(res, "Signed in successfully", {
+            token,
+            user,
+          });
         }
-      } 
+      }
 
       return sendErrorResponse(res, 401, "Invalid credentials provided");
-
     } catch (error) {
-        console.error({name:"Error signing in user", error})
-        
-        return sendErrorResponse(res, 500, errorMessages.internalServerError);
+      console.error({ name: "Error signing in user", error });
+
+      return sendErrorResponse(res, 500, errorMessages.internalServerError);
     }
   }
 }
